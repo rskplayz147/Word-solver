a687e67e07ee24a94861908aad324434eb51c147
rskplayz147 <fitergamerz@gmail.com>
2026-03-05 19:15:37 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,17 @@
 from setuptools import setup, find_packages
 
-with open("README.md", encoding="utf-8") as f:
-    long_desc = f.read()
-
 setup(
-    name="SheekWord",
-    version="1.0.1",
+    name="Sheekwords",
+    version="1.0.0",
     author="Rahul Bhai",
     description="Telegram WordSeek Auto Solver Userbot",
-    long_description=long_desc,
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-
     packages=find_packages(),
-
     include_package_data=True,
-
     install_requires=[
-        "pyrogram>=1.4,<3.0",
-        "tgcrypto>=1.2"
+        "pyrogram",
+        "tgcrypto"
     ],
-
     python_requires=">=3.8",
-
-    keywords=["telegram", "pyrogram", "wordseek", "solver", "userbot"],
-
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
 )
